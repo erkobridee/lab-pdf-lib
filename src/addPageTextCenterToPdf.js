690841fbbaa2dd85c74c20e68938a6a655c37aa0
erkobridee <erko.bridee@gmail.com>
2022-11-11 18:12:38 +0100

    update: render a rectangle with a inner rectangle on the addPageTextCenterToPdf.js

--- src/addPageTextCenterToPdf.js
+++ src/addPageTextCenterToPdf.js
@@ -1,5 +1,11 @@
 const { StandardFonts } = require("pdf-lib");
 
+const {
+  COLOR,
+  getPDFCoordsFromPage,
+  getPDFCoordsInsideRectangle,
+} = require("./helpers/pdf");
+
 /*
   pdf-lib PageSizes
   https://pdf-lib.js.org/docs/api/index#const-pagesizes
@@ -15,19 +21,45 @@ const addPageTextCenterToPdf = async (pdfDoc) => {
 
   // console.log("addPageToPdf > current last page size: ", { width, height });
 
-  const page = pdfDoc.addPage([width, height]);
+  const pdfPage = pdfDoc.addPage([width, height]);
 
   const text = "This text is centered.";
   const textSize = 24;
   const textWidth = helveticaFont.widthOfTextAtSize(text, textSize);
   const textHeight = helveticaFont.heightAtSize(textSize);
 
-  page.drawText(text, {
+  pdfPage.drawText(text, {
     x: width / 2 - textWidth / 2,
     y: height / 2 - textHeight / 2,
     size: textSize,
     font: helveticaFont,
   });
+
+  //---===---//
+
+  const outerRectangleOptions = {
+    color: COLOR.AIR_FORCE_BLUE,
+    ...getPDFCoordsFromPage({
+      x: 50,
+      y: 200,
+      width: 150,
+      height: 100,
+      pdfPage,
+    }),
+  };
+  pdfPage.drawRectangle(outerRectangleOptions);
+
+  const innerRectangleOptions = {
+    color: COLOR.NAVAJO_WHITE,
+    ...getPDFCoordsInsideRectangle({
+      rectangle: outerRectangleOptions,
+      retanglePaddings: 5,
+      width: 60,
+      right: 0,
+      bottom: 0,
+    }),
+  };
+  pdfPage.drawRectangle(innerRectangleOptions);
 
   return pdfDoc;
 };
