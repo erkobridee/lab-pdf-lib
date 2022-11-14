2a48b1de998609ca445860f1d61a2d605817ecb7
erkobridee <erko.bridee@gmail.com>
2022-11-14 14:13:49 +0100

    update(helpers): signatories

--- src/helpers/signatories.js
+++ src/helpers/signatories.js
@@ -7,19 +7,19 @@ const { sha256 } = require("./sha256");
 
 const shortTranslator = short();
 
-const buildSignatory = () => {
-  const name = getRandomName();
+const buildSignatory = (nameSize) => {
+  const name = getRandomName(nameSize);
   const uuid = shortTranslator.uuid();
   const shortId = shortTranslator.fromUUID(uuid);
-  const hash = sha256(JSON.stringify({ name, uuid, shortId }));
+  const hash = sha256({ name, uuid, shortId });
   return { name, uuid, shortId, hash };
 };
 
-const generateSignatories = (min = 3, max = 15) => {
+const generateSignatories = (min = 3, max = 15, nameSize) => {
   const amount = getRandomInt(min, max);
   let signatories = [];
   for (let i = 0; i < amount; i++) {
-    signatories.push(buildSignatory());
+    signatories.push(buildSignatory(nameSize));
   }
   return signatories;
 };
