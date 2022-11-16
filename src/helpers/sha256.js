bb0c805c77f78d4ae18347b86c66acf39c30f75d
erkobridee <erko.bridee@gmail.com>
2022-11-16 16:06:42 +0100

    update(helpers): sha256.js

--- src/helpers/sha256.js
+++ src/helpers/sha256.js
@@ -1,9 +1,13 @@
+// https://nodejs.org/docs/latest-v16.x/api/crypto.html#cryptocreatehashalgorithm-options
+
 const { createHash } = require("node:crypto");
 
 const { isTypeOfObject } = require("./is");
 
+const hashSha256 = createHash("sha256");
+
 const sha256 = (content) =>
-  createHash("sha256")
+  hashSha256
     .update(isTypeOfObject(content) ? JSON.stringify(content) : content)
     .digest("hex");
 
