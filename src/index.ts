419d45b20b768574f379fa785e01b8b70d599342
erkobridee <erko.bridee@gmail.com>
2022-12-08 15:55:15 +0100

    feat: define the src/index.ts code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-const message = "hello world";
+import { generateSignatories } from "@/entities";
 
-console.log(message);
+import { uuid } from "@/utils/data/id";
+import { loadPdfFile, writePdfFile } from "@/utils/pdf";
+
+import { processSignatures, debugAll } from "@/pdflibUtils";
+
+(async () => {
+  const signatories = generateSignatories();
+
+  let pdfFileBuffer = loadPdfFile();
+
+  debugAll();
+
+  pdfFileBuffer = (await processSignatures({
+    rawPdf: pdfFileBuffer,
+    acroformId: uuid(),
+    signatories,
+    saveAsBase64: false,
+  })) as Buffer;
+
+  writePdfFile(pdfFileBuffer, "result_v2");
+})();
